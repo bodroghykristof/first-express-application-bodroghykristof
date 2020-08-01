c445f1051b24130d17479903232c3edbdc84c81e
bodroghykristof <bodroghykristof@gmail.com>
2020-08-01 21:15:59 +0200

    Create metamorphosis with an express.js bacck-end and experiment with request object's properties and middlewares

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
-import Express from "express";
+import express from "express";
 
-const app =  Express();
+const app =  express();
+
+app.use(mid);
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+
 const port = 8000;
 
 app.get('/', (request, response) => {
     console.log('Get');
-    response.json('Hello')
+    response.send('heoh');
+})
+
+app.get('/text/:text', (request, response) => {
+    console.log('Get');
+    console.log(request.params.text);
+    console.log(request.query.name);
+    console.log(request.body.name);
+    response.send('Under construction');
 })
 
 app.listen(port, () => console.log('Listening to port ' + port));
+
+function mid(request, response, next) {
+    console.log("I'm mid");
+    next();
+}
